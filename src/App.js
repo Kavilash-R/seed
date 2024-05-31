ef8452fecf8a56f156c004f2b9f592429d1512fb
Kavilash-R <kavilashravi@gmail.com>
2024-05-31 20:20:03 +0530

    added tailwind css to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import Main from "./components/Main";
 
 function App() {
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Main = () => {
   return (
     <>
-      <div>Main</div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 };
